4e32b2e20086dddcd60317624f2cc202dce7a9af
Robert Kusz <robert.kusz98@gmail.com>
2023-12-15 18:14:33 +0100

    implement webpage that allows to view movies by his genre

---
+++ src/main/java/pl/powtorka/Spring/domain/genre/GenreDtoMapper.java
@@ -0,0 +1,14 @@
+package pl.powtorka.Spring.domain.genre;
+
+
+import pl.powtorka.Spring.domain.genre.dto.GenreDto;
+
+class GenreDtoMapper {
+    static GenreDto map(Genre genre) {
+        return new GenreDto(
+                genre.getId(),
+                genre.getName(),
+                genre.getDescription()
+        );
+    }
+}
---
+++ src/main/java/pl/powtorka/Spring/domain/web/GenreController.java
@@ -0,0 +1,37 @@
+package pl.powtorka.Spring.domain.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import pl.powtorka.Spring.domain.genre.GenreService;
+import pl.powtorka.Spring.domain.genre.dto.GenreDto;
+import pl.powtorka.Spring.domain.movie.MovieService;
+import pl.powtorka.Spring.domain.movie.dto.MovieDto;
+
+import java.util.List;
+
+@Controller
+public class GenreController {
+    private final GenreService genreService;
+    private final MovieService movieService;
+
+    public GenreController(GenreService genreService, MovieService movieService) {
+        this.genreService = genreService;
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/gatunek/{name}")
+    public String getGenre(@PathVariable String name, Model model){
+        GenreDto genre = genreService.findGenreByName(name)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        List<MovieDto> movies = movieService.findMoviesByGenreName(name);
+        model.addAttribute("heading", genre.getName());
+        model.addAttribute("description", genre.getDescription());
+        model.addAttribute("movies", movies);
+        return "movie-listing";
+
+    }
+}
