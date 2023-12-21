61a4486da78e9721505c81f2271f1f38cc7b92c8
Robert Kusz <robert.kusz98@gmail.com>
2023-12-21 17:50:50 +0100

    impement login and logout

--- src/main/java/pl/powtorka/Spring/config/security/CustomSecurityConfig.java
+++ src/main/java/pl/powtorka/Spring/config/security/CustomSecurityConfig.java
@@ -3,9 +3,11 @@ package pl.powtorka.Spring.config.security;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,7 +22,14 @@ public class CustomSecurityConfig {
                         .requestMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
                         .anyRequest().permitAll()
                 )
-                .formLogin(withDefaults());
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                        .logoutSuccessUrl("/login?logout").permitAll()
+                );
         return http.build();
     }
 
